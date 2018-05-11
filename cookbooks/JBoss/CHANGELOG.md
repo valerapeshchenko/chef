082cabcfd7dc38ffbaf88661ee419158054271ba
Valera <valera.peshchenko@gmail.com>
2018-05-11 10:37:58 +0300

    without JBoss

--- cookbooks/JBoss/CHANGELOG.md
+++
@@ -1,9 +0,0 @@
-# 0.1.0
-
-Initial release of jboss7
-
-* Enhancements
-  * an enhancement
-
-* Bug Fixes
-  * a bug fix
--- cookbooks/JBoss/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default['jboss5']['jboss_path'] = '/opt'
-default['jboss5']['jboss_home'] = "#{node['jboss5']['jboss_path']}/jboss"
-default['jboss5']['jboss_user'] = 'web'
-default['jboss5']['jboss_group'] = 'web'
-default['jboss5']['dl_url'] = "https://kent.dl.sourceforge.net/project/jboss/JBoss/JBoss-5.1.0.GA/jboss-5.1.0.GA.zip"
