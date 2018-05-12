088ab8e07b30ffb3c7de573cc575b59e7922a4a1
Valera <valera.peshchenko@gmail.com>
2018-05-12 21:05:43 +0300

    day4 attempt3

--- roles/application.rb
+++ roles/application.rb
@@ -1,4 +1,4 @@
 name "application"
-description "The base role for systems that installs JBoss Server and deploy wars"
+description "for app servers"
 run_list "recipe[jboss5]"
 env_run_lists "Chef-dev" => ["recipe[jboss5]"], "_default" => ["recipe[jboss5]"]
--- roles/nginx.rb
+++ roles/nginx.rb
@@ -1,4 +1,4 @@
 name "nginx"
-description "The base role for systems that installs JBoss Server and deploy wars"
+description "for super web servers"
 run_list "recipe[web]"
 env_run_lists "Chef-dev" => ["recipe[web]"], "_default" => ["recipe[web]"]
